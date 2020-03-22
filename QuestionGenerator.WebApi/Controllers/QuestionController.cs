7814e5fbc3421ae1cdbe7977712cc4cf95d916be
Tanda <tanda.kabanda@asos.com>
2020-03-22 23:16:52 +0000

    Added WebApi project and Get endpoint

---
+++ QuestionGenerator.WebApi/Controllers/QuestionController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using QuestionGenerator.Lib;
+using QuestionGenerator.Lib.Model;
+
+namespace QuestionGenerator.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/question")]
+    public class QuestionController : Controller
+    {
+        private readonly IQuestionGenerator _questionGenerator;
+
+        public QuestionController(IQuestionGenerator questionGenerator)
+        {
+            _questionGenerator = questionGenerator;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Question> Get(int id)
+        {
+            if (!IsValidateQuestionTypeRequestId(id))
+            {
+                return BadRequest(ModelState);
+            }
+
+            var requestedQuestionType = (QuestionType) id;
+            var result = _questionGenerator.ReturnQuestion(requestedQuestionType);
+
+            var question = MapResult(result, requestedQuestionType);
+
+            return Ok(question);
+        }
+
+        private static Question MapResult(string result, QuestionType questionType)
+        {
+            return new Question
+            {
+                QuestionType = questionType.ToString(),
+                question = result
+            };
+        }
+
+        private static bool IsValidateQuestionTypeRequestId(int input)
+        {
+            return input > 0 && input < 5;
+        }
+    }
+}
---
+++ Src/Lib/Models/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace QuestionGenerator.Lib.Model
+{
+    public class Question
+    {
+        public string QuestionType { get; set;}
+        public string question { get; set; }
+    }
+}
